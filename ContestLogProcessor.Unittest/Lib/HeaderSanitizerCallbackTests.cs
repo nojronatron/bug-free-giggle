1ca27a84e35786bf7774c152aedd992d5873077c
Jon Rumsey <jonrumsey.dev@gmail.com>
2025-10-02 11:36:47 -0700

    Initial sanitizer implementations.

---
+++ ContestLogProcessor.Unittest/Lib/HeaderSanitizerCallbackTests.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections.Generic;
+using ContestLogProcessor.Lib;
+using Xunit;
+
+namespace ContestLogProcessor.Unittest.Lib
+{
+    public class HeaderSanitizerCallbackTests
+    {
+        [Fact]
+        public void ImportFile_SanitizesHeaderAndInvokesWarningCallback()
+        {
+            // Arrange: create a temporary cabrillo file with a long header containing a suspicious substring
+            string tempPath = System.IO.Path.GetTempFileName();
+            try
+            {
+                string[] lines = new[]
+                {
+                    "START-OF-LOG: 3.0",
+                    // Make a header value longer than 13 chars and include a suspicious pattern
+                    "NAME: This user tried to run select * from users on import",
+                    "CREATED-BY: Test",
+                    "END-OF-LOG:",
+                };
+                System.IO.File.WriteAllLines(tempPath, lines);
+
+                // Capture warnings
+                List<string> warnings = new List<string>();
+                Action<string> capture = msg => warnings.Add(msg);
+
+                var proc = new CabrilloLogProcessor(capture);
+
+                // Act
+                proc.ImportFile(tempPath);
+
+                // Assert
+                // The header should be present but sanitized in the read-only snapshot
+                var snapshot = proc.GetReadOnlyLogFile();
+                Assert.NotNull(snapshot);
+                Assert.True(snapshot.Headers.ContainsKey("NAME"));
+                string nameVal = snapshot.Headers["NAME"];
+                // The suspicious substring should have been masked (asterisks)
+                Assert.DoesNotContain("select * from", nameVal, StringComparison.OrdinalIgnoreCase);
+
+                // The warning callback should have been invoked at least once
+                Assert.NotEmpty(warnings);
+                Assert.Contains(warnings, w => w.Contains("Sanitized header", StringComparison.OrdinalIgnoreCase));
+            }
+            finally
+            {
+                try { System.IO.File.Delete(tempPath); } catch { }
+            }
+        }
+    }
+}
---
+++ ContestLogProcessor.Unittest/Lib/HeaderSanitizerTests.cs
@@ -0,0 +1,56 @@
+using System;
+using System.IO;
+using Xunit;
+using ContestLogProcessor.Lib;
+
+namespace ContestLogProcessor.Unittest.Lib;
+
+public class HeaderSanitizerTests
+{
+    [Fact]
+    public void SanitizeHeaderValue_Masks_Malicious_Substring()
+    {
+        string tmp = Path.GetTempFileName();
+        try
+        {
+            // Header value contains a suspicious substring 'select * from' and is longer than 13 chars
+            // Use a sanitizable header key (NAME) which our policy covers.
+            File.WriteAllText(tmp, "START-OF-LOG: 3.0\r\nCREATED-BY: innocent\r\nNAME: select * from users where id=1\r\nEND-OF-LOG:\r\n");
+
+            var proc = new CabrilloLogProcessor();
+            proc.ImportFile(tmp);
+
+            Assert.True(proc.TryGetHeader("NAME", out string? noteVal));
+            Assert.NotNull(noteVal);
+            // The suspicious substring should not appear verbatim
+            Assert.DoesNotContain("select * from", noteVal!.ToLowerInvariant());
+            // The sanitized value should contain masked characters ('*') and retain the trailing part
+            Assert.Contains('*', noteVal);
+            Assert.EndsWith(" users where id=1", noteVal);
+        }
+        finally
+        {
+            if (File.Exists(tmp)) File.Delete(tmp);
+        }
+    }
+
+    [Fact]
+    public void SanitizeHeaderValue_Does_Not_Mask_Short_Benign_Values()
+    {
+        string tmp = Path.GetTempFileName();
+        try
+        {
+            File.WriteAllText(tmp, "START-OF-LOG: 3.0\r\nCREATED-BY: ShortName\r\nEND-OF-LOG:\r\n");
+
+            var proc = new CabrilloLogProcessor();
+            proc.ImportFile(tmp);
+
+            Assert.True(proc.TryGetHeader("CREATED-BY", out string? cb));
+            Assert.Equal("ShortName", cb);
+        }
+        finally
+        {
+            if (File.Exists(tmp)) File.Delete(tmp);
+        }
+    }
+}
