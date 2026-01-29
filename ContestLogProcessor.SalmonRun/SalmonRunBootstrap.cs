abc1b9aa8a0eb16214b16b5ff4c233342d174162
Jon Rumsey <jonrumsey.dev@gmail.com>
2026-01-28 17:28:01 -0800

    Add SR bootstrap and location lookup class.

---
+++ ContestLogProcessor.SalmonRun/SalmonRunBootstrap.cs
@@ -0,0 +1,24 @@
+using Microsoft.Extensions.DependencyInjection;
+using ContestLogProcessor.Lib;
+
+namespace ContestLogProcessor.SalmonRun;
+
+/// <summary>
+/// Bootstrap extensions for registering Salmon Run contest services.
+/// </summary>
+public static class SalmonRunBootstrap
+{
+    /// <summary>
+    /// Register Salmon Run contest services with the DI container.
+    /// </summary>
+    /// <param name="services">Service collection to register services with</param>
+    /// <returns>The service collection for method chaining</returns>
+    public static IServiceCollection RegisterSalmonRunContest(this IServiceCollection services)
+    {
+        // Register Salmon Run specific services
+        services.AddSingleton<ILocationLookup, InMemoryLocationLookup>();
+        services.AddSingleton<IContestScoringService<SalmonRunScoreResult>, SalmonRunScoringService>();
+
+        return services;
+    }
+}
